3e9c4eaa427a6fbfe6d60ebc001877180859a91a
Mousab Basil <mousabbasilg@gmail.com>
2024-03-15 15:28:10 +0300

    first commet

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  Footer,
+  Blog,
+  Possibility,
+  WhatGPT3,
+  Header,
+  Features,
+} from "./containers";
+import { CTA, Navbar, Brand } from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import image from "../../assets/possibility.png";
+import "./possibility.css";
+
+const possibility = () => {
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={image} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Requset Early Accesss to Get Started</h4>
+        <h1 className="gradient__text">
+          The Possibilitys are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Requset Early Accesss to Get Started</h4>
+      </div>
+    </div>
+  );
+};
+
+export default possibility;
